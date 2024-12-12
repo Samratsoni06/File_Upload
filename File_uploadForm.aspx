c30fbc2489208fac0e15362db8cdebdd4e97b706
Samratsoni06 <samratsoni800@gmail.com>
2024-12-12 23:48:14 +0530

    Add project files.

---
+++ File_uploadForm.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="File_uploadForm.aspx.cs" Inherits="File_Upload.File_uploadForm" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <hr />
+    <div class="row">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-4">
+                    <asp:Label ID="Msg" runat="server"></asp:Label>
+                    <label>Upload</label>
+                    <asp:FileUpload runat="server" ID="fileupload" CssClass="form-control" />
+                </div>
+                <div class="col-md-4">
+                    <asp:Button runat="server" Text="Upload" ID="btnupload" OnClick="btnupload_Click" CssClass="btn btn-primary" />
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <hr />
+    <div class="col-md-4">
+        <asp:TextBox runat="server" ID="txtimgid" CssClass="form-control"></asp:TextBox>
+        <asp:Button runat="server" Text="ShowImg" ID="btnshow" OnClick="btnshow_Click" CssClass="btn btn-primary" />
+    </div>
+
+    <hr />
+    <div class="col-md-4">
+        <asp:Image ID="img" runat="server" Width="200px" Height="200px" />
+    </div>
+
+</asp:Content>
---
+++ File_uploadForm.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace File_Upload
+{
+    public partial class File_uploadForm : System.Web.UI.Page
+    {
+        string ConStr = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (! IsPostBack)
+            {
+                //Showimg();
+            }
+        }
+
+        protected void btnupload_Click(object sender, EventArgs e)
+        {
+
+            string FileName = fileupload.FileName;
+            fileupload.PostedFile.SaveAs(Server.MapPath("/ImageUpload/" + FileName));
+            string Fpath = "/ImageUpload/" + FileName.ToString();
+
+            SqlConnection con = new SqlConnection(ConStr);
+            SqlCommand cmd = new SqlCommand("_UploadFile", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FileNames", FileName);
+            cmd.Parameters.AddWithValue("@PatnName", Fpath);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+        public void Showimg(string fname)
+        {
+            img.ImageUrl = ("/ImageUpload/" + fname);
+        }
+
+        protected void btnshow_Click(object sender, EventArgs e)
+        {
+            int ID = Convert.ToInt32(txtimgid.Text.Trim());
+
+            string fileNames = string.Empty;
+
+            using (SqlConnection con = new SqlConnection(ConStr))
+            {
+                string query = "SELECT FileNames,PatnName FROM File_upload WHERE ID = @ID ";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@ID", ID);
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        fileNames = reader["FileNames"].ToString();
+                        Showimg(fileNames);
+                    }
+                }
+            }
+        }
+    }
+}
